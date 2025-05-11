a390e42560d5e0a13cecb7707a68dbf17a46ca41
Nikhil Kumar Awasthi <nikhilkumarawasthi11@gmail.com>
2025-05-11 08:59:22 +0530

    Mongo correction

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ app.use(express.json());
 app.use('/api', userRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI,{
+   useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => console.log(" Connected to MongoDB"))
 .catch((err) => console.error(" MongoDB connection error:", err));
 
